dbf454d12b424edbefc90837199c2d7364153fd9
Sahil Gandhi <sahilmgandhi@gmail.com>
2018-05-29 00:42:26 -0700

    Add BLE spec and modify python files to use uuid

--- analyze_data.py
+++ analyze_data.py
@@ -1,27 +1,60 @@
 # Turn the LED on the hexiwear on and off using bluetooth
 
+import binascii
+import struct
 import bluepy.btle
-from bluepy.btle import Peripheral
+from bluepy.btle import Peripheral, UUID
 import time
 
-BT_MAC = '00:3A:40:0A:00:40'
-CMD_TURN_ON = 'ledonledonledonledon'
-CMD_TURN_OFF = 'ledoffledoffledoffle'
+# BT_MAC = '00:3A:40:0A:00:40'
+# CMD_TURN_ON = 'ledonledonledonledon'
+# CMD_TURN_OFF = 'ledoffledoffledoffle'
 
-hexi = Peripheral()
-hexi.connect(BT_MAC)
-ledctl = hexi.getCharacteristics(uuid='2031')[0]  # Alert Input
+# hexi = Peripheral()
+# hexi.connect(BT_MAC)
+# ledctl = hexi.getCharacteristics(uuid='2031')[0]  # Alert Input
 
-for i in range(10):
-    ledctl.write(CMD_TURN_ON, True)
+# for i in range(10):
+#     ledctl.write(CMD_TURN_ON, True)
 
-    j = 0
-    while j < 1000000:
-        j = j + 1
-    ledctl.write(CMD_TURN_OFF, True)
-    j = 0
-    while j < 1000000:
-        j = j + 1
+#     j = 0
+#     while j < 1000000:
+#         j = j + 1
+#     ledctl.write(CMD_TURN_OFF, True)
+#     j = 0
+#     while j < 1000000:
+#         j = j + 1
 
 
-hexi.disconnect()
+def hexStrToInt(hexstr):
+    val = int(hexstr[0:2], 16) + (int(hexstr[2:4], 16) << 8)
+    if ((val & 0x8000) == 0x8000):  # treat signed 16bits
+        val = -((val ^ 0xffff)+1)
+    return val
+
+# hexi.disconnect()
+
+
+# gyro_uuid = UUID(0x2012)
+gyro_uuid = UUID(0x2002)
+
+p = Peripheral()
+p.connect('00:35:40:08:00:48')
+
+try:
+    ch = p.getCharacteristics(uuid=gyro_uuid)[0]
+    print(ch)
+    if (ch.supportsRead()):
+        while 1:
+            # print(ch.read())
+            readChar = ch.read()
+            print(repr(readChar))
+            val = str(binascii.b2a_hex(readChar))
+            print(str(val))
+            print(float(hexStrToInt(val[0:4]))/100),
+            print(float(hexStrToInt(val[4:8]))/100),
+            print(float(hexStrToInt(val[8:12]))/100)
+            time.sleep(1)
+
+finally:
+    p.disconnect()
--- sample.py
+++ sample.py
@@ -32,19 +32,65 @@ def hexStrToInt(hexstr):
 
 # while True:
 # Accelerometer
+child.sendline("char-read-uuid 0x2001")
+child.expect("handle: ", timeout=10)
+child.expect("\r\n", timeout=10)
+trimmedString = child.before[child.before.find(":")+2:]
+print("Accel: "),
+print(trimmedString),
+print(float(hexStrToInt(trimmedString[0:5]))/100),
+print(float(hexStrToInt(trimmedString[6:11]))/100),
+print(float(hexStrToInt(trimmedString[12:17]))/100)
+
+# Gyroscope
+child.sendline("char-read-uuid 0x2002")
+child.expect("handle: ", timeout=10)
+child.expect("\r\n", timeout=10)
+trimmedString = child.before[child.before.find(":")+2:]
+print("Gyro: "),
+print(trimmedString),
+print(float(hexStrToInt(trimmedString[0:5]))/100),
+print(float(hexStrToInt(trimmedString[6:11]))/100),
+print(float(hexStrToInt(trimmedString[12:17]))/100)
+
+# Magnetometer
+child.sendline("char-read-uuid 0x2003")
+child.expect("handle: ", timeout=10)
+child.expect("\r\n", timeout=10)
+trimmedString = child.before[child.before.find(":")+2:]
+print("Magneto:"),
+print(trimmedString),
+print(hexStrToInt(trimmedString[0:5])),
+print(hexStrToInt(trimmedString[6:11])),
+print(hexStrToInt(trimmedString[12:17]))
+
+# while True:
+# Accelerometer
 child.sendline("char-read-hnd 0x30")
 child.expect("Characteristic value/descriptor: ", timeout=10)
 child.expect("\r\n", timeout=10)
-print("Accel: "), print(child.before), print(float(hexStrToInt(child.before[0:5]))/100), print(float(hexStrToInt(child.before[6:11]))/100), print(float(hexStrToInt(child.before[12:17]))/100)
+print("Accel: "),
+print(child.before),
+print(float(hexStrToInt(child.before[0:5])) / 100),
+print(float(hexStrToInt(child.before[6:11])) / 100),
+print(float(hexStrToInt(child.before[12:17]))/100)
 
 # Gyroscope
 child.sendline("char-read-hnd 0x34")
 child.expect("Characteristic value/descriptor: ", timeout=10)
 child.expect("\r\n", timeout=10)
-print("Gyro: "), print(child.before), print(float(hexStrToInt(child.before[0:5]))/100), print(float(hexStrToInt(child.before[6:11]))/100), print(float(hexStrToInt(child.before[12:17]))/100)
+print("Gyro: "),
+print(child.before),
+print(float(hexStrToInt(child.before[0:5])) / 100),
+print(float(hexStrToInt(child.before[6:11])) / 100),
+print(float(hexStrToInt(child.before[12:17]))/100)
 
 # Magnetometer
 child.sendline("char-read-hnd 0x38")
 child.expect("Characteristic value/descriptor: ", timeout=10)
 child.expect("\r\n", timeout=10)
-print("Magneto:"), print(child.before), print(hexStrToInt(child.before[0:5])), print(hexStrToInt(child.before[6:11])), print(hexStrToInt(child.before[12:17]))
+print("Magneto:"),
+print(child.before),
+print(hexStrToInt(child.before[0:5])),
+print(hexStrToInt(child.before[6:11])),
+print(hexStrToInt(child.before[12:17]))
